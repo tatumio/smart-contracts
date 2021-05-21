a8fdc848946b3448a4dcd653d59d60d30a3b5d4a
Rachit-gaur <46561277+Rachit-gaur@users.noreply.github.com>
2021-05-21 14:17:48 +0530

    Tatum1155: Added Erc1155 contract with cashback txns (#4)
    * Modified logic for token transfer cashback
    
    merging safeTransfer and transferCashback
    
    returning unspent value and fixed transfer
    
    adding support for non cb txn
    
    * Added Mint with Cashback logic
    
    * Added Tatum1155 with cashback txns
    
    * Added cashback transfer txn
    
    * Added batch functions
    
    * Added Erc1155 with cashback and reduced bytecode
    
    * Allowance functionality removed
    
    Co-authored-by: rgaur <rachit.gaur@soprasteria.com>
    Co-authored-by: tatumio <48604299+tatumio@users.noreply.github.com>

---
+++ contracts/token/ERC1155/Tatum1155.sol
@@ -0,0 +1,571 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./IERC1155.sol";
+import "./IERC1155Receiver.sol";
+import "./extensions/IERC1155MetadataURI.sol";
+import "../../utils/Address.sol";
+import "../../utils/Context.sol";
+import "../../utils/introspection/ERC165.sol";
+
+contract Tatum1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
+    using Address for address;
+    // Mapping cashbacks and values to tokens
+    mapping(uint256 => address[]) private _cashbackRecipients;
+    mapping(uint256 => uint256[]) private _cashbackValues;
+    // Mapping from token ID to account balances
+    mapping(uint256 => mapping(address => uint256)) private _balances;
+
+    // Mapping from account to operator approvals
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Used as the URI for all token types by relying on ID substitution, e.g. https://token-cdn-domain/{id}.json
+    string private _uri;
+
+    /**
+     * @dev See {_setURI}.
+     */
+    constructor(string memory uri_) {
+        _setURI(uri_);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC165, IERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC1155).interfaceId ||
+            interfaceId == type(IERC1155MetadataURI).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function uri(uint256)
+        external
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return _uri;
+    }
+
+    /**
+     * @dev See {IERC1155-balanceOf}.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     */
+    function balanceOf(address account, uint256 id)
+        public
+        view
+        virtual
+        override
+        returns (uint256)
+    {
+        require(
+            account != address(0),
+            "ERC1155: balance query for the zero address"
+        );
+        return _balances[id][account];
+    }
+
+    /**
+     * @dev See {IERC1155-balanceOfBatch}.
+     *
+     * Requirements:
+     *
+     * - `accounts` and `ids` must have the same length.
+     */
+    function balanceOfBatch(address[] memory accounts, uint256[] memory ids)
+        public
+        view
+        virtual
+        override
+        returns (uint256[] memory)
+    {
+        require(
+            accounts.length == ids.length,
+            "ERC1155: accounts and ids length mismatch"
+        );
+
+        uint256[] memory batchBalances = new uint256[](accounts.length);
+
+        for (uint256 i = 0; i < accounts.length; ++i) {
+            batchBalances[i] = balanceOf(accounts[i], ids[i]);
+        }
+
+        return batchBalances;
+    }
+
+    /**
+     * @dev See {IERC1155-setApprovalForAll}.
+     */
+    function setApprovalForAll(address, bool) public virtual override {
+        require(false, "Not supported");
+    }
+
+    /**
+     * @dev See {IERC1155-isApprovedForAll}.
+     */
+    function isApprovedForAll(address, address)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        require(false, "Not supported");
+        return false;
+    }
+
+    /**
+     * @dev See {IERC1155-safeTransfer}.
+     * Also sends cashback to authors if any
+     */
+    function safeTransfer(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public payable {
+        require(to != address(0), "ERC1155: transfer to the zero address");
+        address from = _msgSender();
+        address operator = _msgSender();
+
+        _beforeTokenTransfer(
+            operator,
+            from,
+            to,
+            _asSingletonArray(id),
+            _asSingletonArray(amount),
+            data
+        );
+
+        uint256 fromBalance = _balances[id][from];
+        require(
+            fromBalance >= amount,
+            "ERC1155: insufficient balance for transfer"
+        );
+        _balances[id][from] = fromBalance - amount;
+        _balances[id][to] += amount;
+        if (_cashbackRecipients[id].length != 0) {
+            uint256 sum = 0;
+            for (uint256 i = 0; i < _cashbackValues[id].length; i++) {
+                sum += _cashbackValues[id][i];
+            }
+            require(
+                msg.value >= sum,
+                "ERC1155: value must be greater than cashback values"
+            );
+            for (uint256 j = 0; j < _cashbackRecipients[id].length; j++) {
+                // transferring cashback to authors
+                payable(_cashbackRecipients[id][j]).transfer(
+                    _cashbackValues[id][j]
+                );
+            }
+            if (msg.value > sum) {
+                payable(msg.sender).transfer(msg.value - sum);
+            }
+        } else {
+            if (msg.value > 0) {
+                payable(msg.sender).transfer(msg.value);
+            }
+        }
+        emit TransferSingle(operator, from, to, id, amount);
+
+        _doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
+    }
+
+    /**
+     * @dev See {IERC1155-safeBatchTransfer}.
+     * Also sends cashback to authors if any
+     */
+    function safeBatchTransfer(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public payable {
+        require(
+            ids.length == amounts.length,
+            "ERC1155: ids and amounts length mismatch"
+        );
+        require(to != address(0), "ERC1155: transfer to the zero address");
+        address from = _msgSender();
+        address operator = _msgSender();
+        uint256 bal = msg.value;
+        //_beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 fromBalance = _balances[id][from];
+            require(
+                fromBalance >= amounts[i],
+                "ERC1155: insufficient balance for transfer"
+            );
+            _balances[id][from] = fromBalance - amount;
+            _balances[id][to] += amount;
+            if (_cashbackRecipients[id].length != 0) {
+                for (uint256 j = 0; j < _cashbackRecipients[id].length; j++) {
+                    // transferring cashback to authors
+                    payable(_cashbackRecipients[id][j]).transfer(
+                        _cashbackValues[id][j]
+                    );
+                    bal = bal - _cashbackValues[id][j];
+                }
+            }
+        }
+        if (bal > 0) {
+            payable(msg.sender).transfer(bal);
+        }
+        emit TransferBatch(operator, from, to, ids, amounts);
+    }
+
+    /**
+     * @dev See {IERC1155-safeTransferFrom}.
+     */
+    function safeTransferFrom(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes memory
+    ) public virtual override {
+        require(false, "Not supported");
+    }
+
+    /**
+     * @dev See {IERC1155-safeBatchTransferFrom}.
+     */
+    function safeBatchTransferFrom(
+        address,
+        address,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) public virtual override {
+        require(false, "Not supported");
+    }
+
+    function _setURI(string memory newuri) internal virtual {
+        _uri = newuri;
+    }
+
+    function _mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) internal virtual {
+        require(account != address(0), "ERC1155: mint to the zero address");
+
+        address operator = _msgSender();
+
+        _beforeTokenTransfer(
+            operator,
+            address(0),
+            account,
+            _asSingletonArray(id),
+            _asSingletonArray(amount),
+            data
+        );
+
+        _balances[id][account] += amount;
+        emit TransferSingle(operator, address(0), account, id, amount);
+
+        _doSafeTransferAcceptanceCheck(
+            operator,
+            address(0),
+            account,
+            id,
+            amount,
+            data
+        );
+    }
+
+    function mint(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) public {
+        _mint(account, id, amount, data);
+    }
+
+    function burn(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) public {
+        _burn(account, id, amount);
+    }
+
+    function burnBatch(
+        address account,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) public {
+        _burnBatch(account, ids, amounts);
+    }
+
+    function mintBatch(
+        address[] memory to,
+        uint256[][] memory ids,
+        uint256[][] memory amounts,
+        bytes memory data
+    ) public {
+        for (uint256 i = 0; i < ids.length; i++) {
+            _mintBatch(to[i], ids[i], amounts[i], data);
+        }
+    }
+
+    function mintWithCashback(
+        address account,
+        uint256 id,
+        uint256 amount,
+        bytes memory data,
+        address[] memory authorAddresses,
+        uint256[] memory cashbackValues
+    ) public {
+        // saving cashback addresses and values
+        _cashbackRecipients[id] = authorAddresses;
+        _cashbackValues[id] = cashbackValues;
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatchWithCashback(
+        address[] memory to,
+        uint256[][] memory ids,
+        uint256[][] memory amounts,
+        bytes memory data,
+        address[][][] memory authorAddresses,
+        uint256[][][] memory cashbackValues
+    ) public {
+        for (uint256 i = 0; i < ids.length; i++) {
+            _mintBatch(to[i], ids[i], amounts[i], data);
+            for (uint256 j = 0; j < ids[i].length; j++) {
+                _cashbackRecipients[ids[i][j]] = authorAddresses[i][j];
+                _cashbackValues[ids[i][j]] = cashbackValues[i][j];
+            }
+        }
+    }
+
+    /**
+     * Fetch cashback values and recipients for a token id, returns array
+     */
+    function tokenCashbackValues(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (uint256[] memory)
+    {
+        return _cashbackValues[tokenId];
+    }
+
+    function tokenCashbackRecipients(uint256 tokenId)
+        public
+        view
+        virtual
+        returns (address[] memory)
+    {
+        return _cashbackRecipients[tokenId];
+    }
+
+    /**
+     * To update the cashback values of an existing author, returns bool
+     */
+    function updateCashbackForAuthor(uint256 tokenId, uint256 cashbackValue)
+        public
+        returns (bool)
+    {
+        for (uint256 i = 0; i < _cashbackValues[tokenId].length; i++) {
+            if (_cashbackRecipients[tokenId][i] == _msgSender()) {
+                _cashbackValues[tokenId][i] = cashbackValue;
+                return true;
+            }
+        }
+        return true;
+    }
+
+    function _mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual {
+        require(to != address(0), "ERC1155: mint to the zero address");
+        require(
+            ids.length == amounts.length,
+            "ERC1155: ids and amounts length mismatch"
+        );
+
+        address operator = _msgSender();
+
+        _beforeTokenTransfer(operator, address(0), to, ids, amounts, data);
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            _balances[ids[i]][to] += amounts[i];
+        }
+
+        emit TransferBatch(operator, address(0), to, ids, amounts);
+
+        _doSafeBatchTransferAcceptanceCheck(
+            operator,
+            address(0),
+            to,
+            ids,
+            amounts,
+            data
+        );
+    }
+
+    function _burn(
+        address account,
+        uint256 id,
+        uint256 amount
+    ) internal virtual {
+        require(account != address(0), "ERC1155: burn from the zero address");
+
+        address operator = _msgSender();
+
+        _beforeTokenTransfer(
+            operator,
+            account,
+            address(0),
+            _asSingletonArray(id),
+            _asSingletonArray(amount),
+            ""
+        );
+
+        uint256 accountBalance = _balances[id][account];
+        require(
+            accountBalance >= amount,
+            "ERC1155: burn amount exceeds balance"
+        );
+        _balances[id][account] = accountBalance - amount;
+
+        emit TransferSingle(operator, account, address(0), id, amount);
+    }
+
+    function _burnBatch(
+        address account,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    ) internal virtual {
+        require(account != address(0), "ERC1155: burn from the zero address");
+        require(
+            ids.length == amounts.length,
+            "ERC1155: ids and amounts length mismatch"
+        );
+
+        address operator = _msgSender();
+
+        _beforeTokenTransfer(operator, account, address(0), ids, amounts, "");
+
+        for (uint256 i = 0; i < ids.length; i++) {
+            uint256 id = ids[i];
+            uint256 amount = amounts[i];
+
+            uint256 accountBalance = _balances[id][account];
+            require(
+                accountBalance >= amount,
+                "ERC1155: burn amount exceeds balance"
+            );
+            _balances[id][account] = accountBalance - amount;
+        }
+
+        emit TransferBatch(operator, account, address(0), ids, amounts);
+    }
+
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual {}
+
+    function _doSafeTransferAcceptanceCheck(
+        address operator,
+        address from,
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes memory data
+    ) private {
+        if (to.isContract()) {
+            try
+                IERC1155Receiver(to).onERC1155Received(
+                    operator,
+                    from,
+                    id,
+                    amount,
+                    data
+                )
+            returns (bytes4 response) {
+                if (
+                    response != IERC1155Receiver(to).onERC1155Received.selector
+                ) {
+                    revert("ERC1155: ERC1155Receiver rejected tokens");
+                }
+            } catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("ERC1155: transfer to non ERC1155Receiver implementer");
+            }
+        }
+    }
+
+    function _doSafeBatchTransferAcceptanceCheck(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) private {
+        if (to.isContract()) {
+            try
+                IERC1155Receiver(to).onERC1155BatchReceived(
+                    operator,
+                    from,
+                    ids,
+                    amounts,
+                    data
+                )
+            returns (bytes4 response) {
+                if (
+                    response !=
+                    IERC1155Receiver(to).onERC1155BatchReceived.selector
+                ) {
+                    revert("ERC1155: ERC1155Receiver rejected tokens");
+                }
+            } catch Error(string memory reason) {
+                revert(reason);
+            } catch {
+                revert("ERC1155: transfer to non ERC1155Receiver implementer");
+            }
+        }
+    }
+
+    function _asSingletonArray(uint256 element)
+        private
+        pure
+        returns (uint256[] memory)
+    {
+        uint256[] memory array = new uint256[](1);
+        array[0] = element;
+
+        return array;
+    }
+}
