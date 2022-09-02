512ce8deade6f7045fc9cc97c1b8718384c91a44
Damilola Fagoyinbo <fagoyinbo.damilola@tatum.io>
2022-09-02 23:25:24 +0300

    Removed console.log on hardhat

--- contracts/tatum/custodial/CustodialWalletFactoryV2.sol
+++ contracts/tatum/custodial/CustodialWalletFactoryV2.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.7;
 import "../../access/Ownable.sol";
 import "./CustodialWallet.sol";
 import "../../proxy/Clones.sol";
-import "hardhat/console.sol";
 
 contract CustodialWalletFactoryV2 {
 
