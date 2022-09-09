052dfd715a563e1b666afa24d2a09de0b3833ba6
Damilola Fagoyinbo <fagoyinbo.damilola@tatum.io>
2022-09-09 11:02:13 +0300

    Added Factory for Tron chain

--- contracts/proxy/Clones.sol
+++ contracts/proxy/Clones.sol
@@ -69,10 +69,28 @@ library Clones {
         }
     }
 
+    function predictDeterministicTronAddress(address implementation, bytes32 salt, address deployer) internal pure returns (address predicted) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let ptr := mload(0x40)
+            mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(add(ptr, 0x14), shl(0x60, implementation))
+            mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf34100000000000000000000000000000000)
+            mstore(add(ptr, 0x38), shl(0x60, deployer))
+            mstore(add(ptr, 0x4c), salt)
+            mstore(add(ptr, 0x6c), keccak256(ptr, 0x37))
+            predicted := keccak256(add(ptr, 0x37), 0x55)
+        }
+    }
+
     /**
      * @dev Computes the address of a clone deployed using {Clones-cloneDeterministic}.
      */
     function predictDeterministicAddress(address implementation, bytes32 salt) internal view returns (address predicted) {
         return predictDeterministicAddress(implementation, salt, address(this));
+    }
+
+    function predictDeterministicTronAddress(address implementation, bytes32 salt) internal view returns (address predicted) {
+        return predictDeterministicTronAddress(implementation, salt, address(this));
     }
 }
